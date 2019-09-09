6dc10bbce0a7899ae1018c8ae2e8e0565eea29a1
Greg Randall <gregrr@gmail.com>
2019-09-09 11:56:03 -0500

    make sure that the output folder is created

--- process.php
+++ process.php
@@ -1,13 +1,20 @@
 <?php
     include('functions.php');
+
     $tags = '';
     foreach ($_POST['tags'] as $tag) {
         $tags .= trim($tag) . "; ";
     }
+
     echo dispay_iptc($image);
+    
     $tags .= $_POST['manual_tags'];
     $tags_written = write_tags($_POST['file_name'], $tags);
     
+    if(!file_exists ('completed_images')){
+        mkdir('completed_images');
+    }
+
     rename ($_POST['file_name'],'completed_images/'.$_POST['file_name']);
 
     echo "tags written: $tags_written<br><br><a href='.'>Tag Another</a>";
